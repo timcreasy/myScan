c6d0c35c4fdbbcd527229919703ddeb71685c3d7
Tim Creasy <timcreasy@me.com>
2016-07-09 23:44:26 -0500

    Add product implemented

--- www/controllers/AddProductCtrl.js
+++ www/controllers/AddProductCtrl.js
@@ -1,23 +1,22 @@
 myScan.controller('AddProductCtrl', ['$scope','$firebaseArray','$ionicPlatform', 'productService',
    function($scope, $firebaseArray, $ionicPlatform, productService) {
 
-     // Get reference to inventory in firebase
-     var inventoryRef = new Firebase("https://myscan.firebaseio.com/inventory");
+    // Get reference to inventory in firebase
+    var inventoryRef = new Firebase("https://myscan.firebaseio.com/inventory");
 
      // Get barcode to add product information to
      var barcode = productService.getProductToAdd();
 
-     // Set input field to barcode
-     document.getElementById('barcodeInput').value = barcode;
+    // Set input field to barcode
+    document.getElementById('barcodeInput').value = barcode;
 
+    $scope.addProductPressed = function() {
 
-     $scope.addProductPressed = function() {
-       console.log("add");
-       var productName = document.getElementById('productName').value;
-       var productQuantity = document.getElementById('productQuantity').value;
+     var productName = document.getElementById('productName').value;
+     var productQuantity = document.getElementById('productQuantity').value;
+     inventoryRef.push({ 'productName': productName, 'quantity': productQuantity, 'barcode': barcode });
 
-       inventoryRef.push({ 'productName': productName, 'quantity': productQuantity, 'barcode': barcode });
-     };
+    };
 
 
    }
